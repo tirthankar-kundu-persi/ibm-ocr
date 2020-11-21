98de53c3003bbc3f23e853722b9bbae69fcdc77d
Tirthankar <tirthankar.kundu94@gmail.com>
2020-11-22 02:09:57 +0530

    First commit

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, jsonify
 import atexit
 import os
 import json
+import tesserocr
+from PIL import Image
 
 app = Flask(__name__, static_url_path='')
 
@@ -10,37 +12,42 @@ db_name = 'mydb'
 client = None
 db = None
 
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.getenv('VCAP_SERVICES'))
-    print('Found VCAP_SERVICES')
-    if 'cloudantNoSQLDB' in vcap:
-        creds = vcap['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
-elif "CLOUDANT_URL" in os.environ:
-    client = Cloudant(os.environ['CLOUDANT_USERNAME'], os.environ['CLOUDANT_PASSWORD'], url=os.environ['CLOUDANT_URL'], connect=True)
-    db = client.create_database(db_name, throw_on_exists=False)
-elif os.path.isfile('vcap-local.json'):
-    with open('vcap-local.json') as f:
-        vcap = json.load(f)
-        print('Found local VCAP_SERVICES')
-        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
+# if 'VCAP_SERVICES' in os.environ:
+#     vcap = json.loads(os.getenv('VCAP_SERVICES'))
+#     print('Found VCAP_SERVICES')
+#     if 'cloudantNoSQLDB' in vcap:
+#         creds = vcap['cloudantNoSQLDB'][0]['credentials']
+#         user = creds['username']
+#         password = creds['password']
+#         url = 'https://' + creds['host']
+#         client = Cloudant(user, password, url=url, connect=True)
+#         db = client.create_database(db_name, throw_on_exists=False)
+# elif "CLOUDANT_URL" in os.environ:
+#     client = Cloudant(os.environ['CLOUDANT_USERNAME'], os.environ['CLOUDANT_PASSWORD'], url=os.environ['CLOUDANT_URL'], connect=True)
+#     db = client.create_database(db_name, throw_on_exists=False)
+# elif os.path.isfile('vcap-local.json'):
+#     with open('vcap-local.json') as f:
+#         vcap = json.load(f)
+#         print('Found local VCAP_SERVICES')
+#         creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
+#         user = creds['username']
+#         password = creds['password']
+#         url = 'https://' + creds['host']
+#         client = Cloudant(user, password, url=url, connect=True)
+#         db = client.create_database(db_name, throw_on_exists=False)
 
 # On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 8000))
+api = tesserocr.PyTessBaseAPI()
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    pil_image = Image.open('1.png')
+    api.SetImage(pil_image)
+    text = api.GetUTF8Text()
+    return jsonify({'text': text})
+    # return app.send_static_file('index.html')
 
 # /* Endpoint to greet and add a new visitor to database.
 # * Send a POST request to localhost:8000/api/visitors with body
